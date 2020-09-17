b6ec61209a6d0b51b757528e97cc6aff9c1f99a7
Sarah Maris <54687857+sarah-udacity@users.noreply.github.com>
2020-09-17 08:19:46 -0400

    Update names

--- db/udaconnect_public_person.sql
+++ db/udaconnect_public_person.sql
@@ -1,5 +1,5 @@
-insert into public.person (id, first_name, last_name, company_name) values (5, 'Nacho', 'Varga', 'A-Z Fine Upholstery');
-insert into public.person (id, first_name, last_name, company_name) values (6, 'Hank', 'Schrader', 'DEA');
-insert into public.person (id, first_name, last_name, company_name) values (1, 'Lalo', 'Salamanca', 'Juarez C');
-insert into public.person (id, first_name, last_name, company_name) values (8, 'Saul', 'Goodman', 'Saul Goodman & Associates');
-insert into public.person (id, first_name, last_name, company_name) values (9, 'Gustavo', 'Fring', 'Los Pollos Hermanos');
+insert into public.person (id, first_name, last_name, company_name) values (5, 'Taco', 'Fargo', 'Alpha Omega Upholstery');
+insert into public.person (id, first_name, last_name, company_name) values (6, 'Frank', 'Shader', 'USDA');
+insert into public.person (id, first_name, last_name, company_name) values (1, 'Pam', 'Trexler', 'Hampton, Hampton and McQuill');
+insert into public.person (id, first_name, last_name, company_name) values (8, 'Paul', 'Badman', 'Paul Badman & Associates');
+insert into public.person (id, first_name, last_name, company_name) values (9, 'Otto', 'Spring', 'The Chicken Sisters Restaurant');
