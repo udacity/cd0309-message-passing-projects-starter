75f90e17aaa5742ed025d08c392b3b06682a8d5e
sumanbgl <suman.bgl@gmail.com>
2021-12-24 14:16:28 +0530

    using 30100 as server port.

--- modules/locationgRPC/location_client.py
+++ modules/locationgRPC/location_client.py
@@ -4,7 +4,7 @@ import location_pb2_grpc
 
 print("Sending location payload ....")
 
-channel = grpc.insecure_channel("localhost:5005")
+channel = grpc.insecure_channel("localhost:30100")
 stub = location_pb2_grpc.LocationServiceStub(channel)
 
 location = location_pb2.LocationMessage(
